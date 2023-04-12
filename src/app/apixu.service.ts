91ebb8ddf376835e8ba351950f9bc0e966a9ec93
Amritaya Ray <77060538+amritayaray02@users.noreply.github.com>
2023-04-12 19:10:16 +0530

    Update apixu.service.ts

--- src/app/apixu.service.ts
+++ src/app/apixu.service.ts
@@ -9,12 +9,12 @@ export class ApixuService {
   constructor(private http: HttpClient) { }
   getlatlong(location){
     return this.http.get(
-      'http://api.openweathermap.org/geo/1.0/direct?q='+location+'&appid=50ca38d7434086e134b8ae35aa0b5414'
+      'http://api.openweathermap.org/geo/1.0/direct?q='+location+'&appid=APIKEY'
     )
   }
   getWeather(lat, lon){
       return this.http.get(
-          'https://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+lon+'&appid=50ca38d7434086e134b8ae35aa0b5414&units=metric'
+          'https://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+lon+'&appid=APIKEY&units=metric'
       );
   }
 }
